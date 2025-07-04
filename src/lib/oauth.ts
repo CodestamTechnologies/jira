dd10d068be886e2f848ef8d39f2c51f979c9566a
sourav mishra <souravvmishra@gmail.com>
2025-07-04 12:46:42 +0530

    done

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -9,8 +9,9 @@ export async function onOAuth(provider: OAuthProvider.Github | OAuthProvider.Goo
   const { account } = await createAdminClient();
 
   const origin = process.env.NEXT_PUBLIC_APP_BASE_URL;
-
-  const redirectUrl = await account.createOAuth2Token(provider, `${origin}/api/auth`, `${origin}/sign-in`);
-
+  console.log('Origin:', account);
+  console.log('Redirect URL:', provider, origin);
+  console.log('Redirect URL:', `${origin}/api/auth`, `${origin}/sign-in`);
+  const redirectUrl = await account.createOAuth2Token(provider);
   return redirect(redirectUrl);
 }
